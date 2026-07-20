8e2bda23f57d9ab5178ee65f03482c394e1fb81c
dlaur-ed <drobertlaur@gmail.com>
2026-07-20 16:43:40 +0300

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,10 +1,12 @@
-const CACHE_NAME = "goobert-kitchen-v1";
+const CACHE_NAME = "goobert-kitchen-v2";
 const APP_SHELL = [
   "./",
   "./index.html",
   "./manifest.json",
   "./icons/icon-192.png",
-  "./icons/icon-512.png"
+  "./icons/icon-512.png",
+  "./icons/icon-192-maskable.png",
+  "./icons/icon-512-maskable.png"
 ];
 
 self.addEventListener("install", (event) => {
